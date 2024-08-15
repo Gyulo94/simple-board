bf030d976ee0fea137fe4fac48fba0e051768e7e
Gyulo94 <qkrcjfrb112@gmail.com>
2024-08-15 19:34:54 +0900

    PostgresSQL 설치 및 셋팅, TypeORM Entity 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
@@ -6,7 +7,20 @@ import ConfigModule from './config';
 import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
-  imports: [ConfigModule(), BoardModule],
+  imports: [
+    ConfigModule(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
