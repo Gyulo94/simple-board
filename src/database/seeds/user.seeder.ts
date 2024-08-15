2dc9b340c492b55bb4281c5b4731abeaa39a077d
Gyulo94 <qkrcjfrb112@gmail.com>
2024-08-15 23:16:39 +0900

    TypeORM Seeding 을 통한 초기 데이터 생성

---
+++ src/database/seeds/user.seeder.ts
@@ -0,0 +1,26 @@
+import { User } from 'src/entity/user.entity';
+import { DataSource } from 'typeorm';
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+
+export default class UserSeeder implements Seeder {
+  /**
+   * Track seeder execution.
+   *
+   * Default: false
+   */
+  track = false;
+
+  public async run(
+    dataSource: DataSource,
+    factoryManager: SeederFactoryManager,
+  ): Promise<any> {
+    const repository = dataSource.getRepository(User);
+    await repository.insert([
+      {
+        username: 'gyulo94',
+        password: '1234',
+        name: 'chalgyu',
+      },
+    ]);
+  }
+}
